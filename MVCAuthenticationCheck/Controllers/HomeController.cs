324affc9ce5a1bb111521f355a82ea99a354609b
OVI <OVI@DESKTOP-DF5TG5J>
2017-08-06 00:56:55 +0600

    x

--- MVCAuthenticationCheck/Controllers/HomeController.cs
+++ MVCAuthenticationCheck/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MVCAuthenticationCheck.Controllers
         {
             return View(); 
         }
-
+         
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
